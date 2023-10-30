0c6ca9580005225fcc634d3d2aef55b01a0822d8
rjgohar <rehmatjan143@gmail.com>
2023-10-30 20:31:54 +0500

    feat:fuccc

--- src/components/aboutUs/index.jsx
+++ src/components/aboutUs/index.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import about from './aboutus.png'
 import icon from './icon1.png'
+import 'aos/dist/aos.css'
 export default function AboutUs() {
   return (
     <section className='bg-black text-white flex flex-col gap-y-20 px-10 justify-center tablet:gap-x-20 py-20 tablet:px-20 tablet:flex-row '>
-      <div>
+      <div data-aos='fade-right' data-aos-duration='300' data-aos-offset='0 '>
         <img className='tablet:h-[600px] tablet:w-[500px]' src={about} alt='aboutus' />
       </div>
-      <div className=' flex flex-col pt-20  w-full tablet:w-5/12'>
+      <div
+        data-aos='fade-left'
+        data-aos-duration='300'
+        data-aos-offset='600'
+        className=' flex flex-col pt-20  w-full tablet:w-5/12'
+      >
         <div className='flex py-4'>
           <img src={icon} alt='icon' />
           <p className='text-sm'>About UDS Studio</p>
@@ -29,7 +35,7 @@ export default function AboutUs() {
           </p>
         </div>
 
-        <div className='   pt-3'>
+        <div className='pt-3'>
           <button>conatct Us</button>
         </div>
       </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
+import AboutUs from 'src/components/aboutUs'
 import { GuardWrapper } from 'src/layouts/guardWrapper'
 
 export default function HomePage(props) {
   return (
     <GuardWrapper {...props}>
       <h1>Home Page</h1>
+      <AboutUs />
     </GuardWrapper>
   )
 }

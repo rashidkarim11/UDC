0094e15e697887a4c28aa40e87758b436616faf1
ZuhaibHunzai <zuhaibalamhunzai188@gmail.com>
2023-10-30 20:54:49 +0500

    experties section created

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,13 @@
 import { GuardWrapper } from 'src/layouts/guardWrapper'
 
+// ** Components Imports
+import ExpertiesSection from 'src/components/experties' 
+
 export default function HomePage(props) {
   return (
     <GuardWrapper {...props}>
       <h1>Home Page</h1>
+      <ExpertiesSection /> 
     </GuardWrapper>
   )
 }

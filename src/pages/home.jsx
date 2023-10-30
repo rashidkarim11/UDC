9119004000d6d8003e7efcb92303f26124f0f137
NoumanHunzai <noumanhunzai222@gmail.com>
2023-10-30 20:10:35 +0500

    footer section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
+import Footer from 'src/components/footer'
 import { GuardWrapper } from 'src/layouts/guardWrapper'
 
 export default function HomePage(props) {
   return (
     <GuardWrapper {...props}>
       <h1>Home Page</h1>
+      <Footer />
     </GuardWrapper>
   )
 }

8ee8e3159e5f233611abc07e34a9c8bef57e4d58
rjgohar <rehmatjan143@gmail.com>
2023-11-01 22:01:57 +0500

    eee

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import { Outlet, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import { ErrorBoundary } from 'src/components/errorBoundary'
 import { SuspenseLayout } from 'src/layouts/suspense.layout'
-<<<<<<< HEAD
-=======
 import Production from 'src/pages/production'
->>>>>>> a1889e470a3cef208e2f51941bfee9b7ce1ad731
 
 const HomePage = lazy(() => import('src/pages/home'))
 const PressKitRelease = lazy(() => import('src/pages/pressKitRelease'))

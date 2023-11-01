d1e4b9027fa2329c763f27fd745aef2dc2a56432
NoumanHunzai <noumanhunzai222@gmail.com>
2023-11-01 23:25:18 +0500

    feat: add the client page

--- src/pages/ourClient.jsx
+++ src/pages/ourClient.jsx
@@ -4,12 +4,14 @@ import { Header } from 'src/components/header'
 import Footer from 'src/components/footer'
 import { GuardWrapper } from 'src/layouts/guardWrapper'
 import { Main } from 'src/views/client/main'
+import Category from 'src/views/client/category'
 
 export default function OurClient(props) {
   return (
     <GuardWrapper {...props}>
       <Header />
       <Main />
+      <Category />
       <Footer />
     </GuardWrapper>
   )
--- src/views/client/main/index.jsx
+++ src/views/client/main/index.jsx
@@ -1,22 +1,33 @@
 import { MainSectionIcon } from 'src/icons'
 import client1 from 'src/assets/images/client-1.png'
 import client2 from 'src/assets/images/client-2.png'
+import { MoonIcon } from 'src/icons/moon'
 
 export const Main = () => {
   return (
-    <section className='min-h-screen h-full relative main-bg '>
+    <section className='min-h-screen h-full relative bg-[#181717] '>
       <div className='h-full min-h-screen flex flex-col justify-start  max-w-7xl m-auto px-4'>
         <div>
-          <div className='max-w-5xl ' data-aos='fade-right' data-aos-duration='800' data-aos-offset='0 '>
-            <h2 className='font-bold text-6xl sm:text-7xl mt-[1.2em] tablet:text-9xl text-white'>
+          <div className='max-w-5xl' data-aos='fade-right' data-aos-duration='800' data-aos-offset='0'>
+            <h2 className='relative font-bold text-6xl sm:text-7xl mt-[1.2em] tablet:text-9xl text-white'>
+              <div className='invisible tablet:visible tablet:absolute tablet:-z-50 tablet:-top-10  tablet:left-40 '>
+                <MoonIcon />
+              </div>
               A snapshot of our portfolio
+              <div className='invisible tablet:visible tablet:absolute tablet:z-50 tablet:right-60 tablet:bottom-4'>
+                <MoonIcon />
+              </div>
             </h2>
-            <div>
-              <div className='flex relative'>
-                <img src={client1} alt='client 1' />
-                <div className='absolute right-0 bottom-0 -z-10 '>
-                  <img src={client2} alt='client 2' />
-                </div>
+
+            <div className='flex relative'>
+              <img src={client1} alt='client 1' />
+              <div
+                className='absolute right-0 bottom-0 -z-10  '
+                data-aos='fade-up'
+                data-aos-duration='800'
+                data-aos-delay='300'
+              >
+                <img src={client2} alt='client 2' />
               </div>
             </div>
           </div>
